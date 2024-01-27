e669064579fd4a0028b8e45dd9955719e9950fa3
Gunawardhane <yasangawd@gmail.com>
2024-01-27 23:46:49 +0530

    Hero.jsx completed

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from './components/Hero.jsx'
+import Demo from './components/Demo.jsx'
+
+import './App.css';
+
+const App = () => {
+  return (
+      <main>
+          <div className='main'>
+              <div className='gradient'/>
+          </div>
+          
+              <div className='app'>
+                  <Hero />
+                  <Demo />
+              
+          </div>
+   </main>
+  )
+}
+
+export default App
