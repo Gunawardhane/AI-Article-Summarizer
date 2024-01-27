3ac05e15de36f5d65f322264cc8b0ff7d45fbef8
Gunawardhane <yasangawd@gmail.com>
2024-01-28 01:44:38 +0530

    article summarizer v1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import Hero from './components/Hero.jsx'
-import Demo from './components/Demo.jsx'
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
-      <main>
-          <div className='main'>
-              <div className='gradient'/>
-          </div>
-          
-              <div className='app'>
-                  <Hero />
-                  <Demo />
-              
-          </div>
-   </main>
-  )
-}
+    <main>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
 
-export default App
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
+
+export default App;
